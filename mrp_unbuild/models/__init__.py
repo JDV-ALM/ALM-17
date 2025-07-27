b890163529bde42597b81eb68cf9356f8bae6eda
Jesus V <jveliz@almus.dev>
2025-07-27 14:33:15 -0400

    Delete

--- mrp_unbuild/models/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-
-from . import mrp_bom_unbuild
-from . import mrp_production_unbuild
-from . import stock_move_unbuild
-from . import mrp_account_unbuild
-from . import stock_warehouse
-from . import res_config_settings  # Agregado archivo faltante
